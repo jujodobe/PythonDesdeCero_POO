0f458c456249817074a2a6951bf1cc0fb645f030
Juan Dos Santos <jds.infosystem@gmail.com>
2022-12-01 18:57:00 -0300

    Nuevo algoritmo sobre cuentas

--- main.py
+++ main.py
@@ -8,6 +8,28 @@ from Objetos.RelacionEntreClases import Ciudad
 from Objetos.Persona import Personas
 from Cuentas.Cuenta import Cuenta
 
+cuenta1 = Cuenta("123456", "Juan Perez", 100000, 1000000)
+cuenta2 = Cuenta("456789", "Maria Gomez", 50000, 2000000)
+
+continuidad = 1
+
+while continuidad == 1:
+    operacion = int(input("Que operacion desea realizar? \n 1- Deposito \n 2- retiro \n 3- Extracto \n Ingrese el Valor: "))
+    if operacion == 1:
+        cuenta1.deposita(int(input("Ingrese la cantidad a depositar: ")))
+
+    if operacion == 2:
+        cuenta1.retiro(int(input("Ingrese la cantidad a depositar: ")))
+
+    if operacion == 3:
+        cuenta1.extracto()
+
+    continuidad = int(input("Desea continuar con las operaciones? \n 1 Afirmativo \n 2- Negativo \n Ingrese valor: "))
+
+
+
+
+"""
 print("\n DATOS DE LA CUENTA 1")
 cuenta1 = Cuenta("123456", "Juan Perez", 100000, 1000000)
 
@@ -33,7 +55,7 @@ print("\n")
 cuenta2.retiro(int(input("ingrese la cantidad a retirar: ")))
 print("\n")
 cuenta2.extracto()
-
+"""
 
 """
 RELACION ENTRE CLASES
